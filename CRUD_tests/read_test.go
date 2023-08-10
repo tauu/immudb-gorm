c6070b174095176fa44e54c57e587fdd6c7317ff
Georg Wechslberger <g.wechslberger@net-q.com>
2023-08-10 13:49:03 +0200

    test: check that the limit clause works

--- CRUD_tests/read_test.go
+++ CRUD_tests/read_test.go
@@ -45,3 +45,35 @@ func TestRead(t *testing.T) {
 	assert.Equal(t, newUser.Name, users[0].Name, "The queried name does not match the defined name")
 	assert.Equal(t, newUser.Age, users[0].Age, "The queried age does not match the defined age")
 }
+
+func TestLimit(t *testing.T) {
+
+	// Open connection
+	db, err := OpenConnection(t)
+	require.NoError(t, err, "There was an error opening connection")
+
+	// Create a users table
+	err = db.AutoMigrate(&User{})
+	require.NoError(t, err, "There was an error creating users table")
+
+	// Define a new record
+	var newUser1 = User{Name: "Jose", Age: 33}
+	var newUser2 = User{Name: "Dave", Age: 35}
+
+	// Create a new user record
+	err = db.Create(&newUser1).Error
+	require.NoError(t, err, "An error occurred while creating the first user")
+	err = db.Create(&newUser2).Error
+	require.NoError(t, err, "An error occurred while creating the second user")
+
+	// Query the data previously inserted to the database
+	var users []User
+
+	err = db.Debug().Limit(1).Find(&users).Error
+	if err != nil {
+		log.Error().Err(err).Msg("No users found")
+	}
+
+	// Test cases
+	assert.Len(t, users, 1, "The query should only return one row if the limit is 1")
+}
